eb13edc09aabcb41caf90a1350fd6b634de5c21b
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-05-08 11:38:57 +1200

    first

---
+++ Methods-2/CAR/CAR/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "Mitsubishi";
+            car1.Model = "Evo IX";
+            car1.Rego = "ABC123";
+            car1.Colour = "Black";
+
+            Console.WriteLine($"Car details: \nMake:{car1.Make}\nModel:{car1.Model}\nRego:{car1.Rego}\nColour:{car1.Colour}");
+        }
+    }
+    class Car
+    {//Attributes
+        string make;
+        string model;
+        string rego;
+        string colour;
+
+        // getters and setters
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public string Rego { get { return rego; } set { rego = value; } }
+        public string Colour { get { return colour; } set { colour = value; } }
+    }
+}
---
+++ Methods-2/Methods-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //4 question
+            Console.WriteLine("enter a no. from 1 to 7");
+            Console.WriteLine("The day is :" + Day(int.Parse(Console.ReadLine())));
+            
+
+
+            //task 1
+            Console.WriteLine("Enter your body weight in kilograms: ");
+            double w = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height in centimetres: ");
+            double h = double.Parse(Console.ReadLine());
+            double bmi = Bmi(w, h);
+            Console.WriteLine($"Your BMI is : {bmi}");
+            Console.ReadLine();
+
+            //task 2
+            Console.WriteLine("Your BMI category is : ");
+
+        }
+
+        static string Day(int num)
+        { string[] days = { "monday", "tuesday", "wednesday", "Thursday", "friday", "saturday", "sunday" };
+            return days[num-1]; }
+
+        static double Bmi(double w, double h)
+        { return Math.Round( w / (h * h) ,1); }
+
+      
+
+    }
+}
